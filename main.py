79e04b4d8c19107dc5b20728721cdaa79815cddf
Your Name <adiboy_3112@kgpian.iitkgp.ac.in>
2022-07-02 15:40:16 +0530

    Added some more helper function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from datetime import timezone
+import traceback
 import alpaca_trade_api as tradeapi
+import tools
+import datetime
 
 API_KEY_ID=""
 API_SECRET_KEY=""
@@ -8,9 +12,45 @@ BASE_URL=""
 def main():
     global API_KEY_ID, API_SECRET_KEY, BASE_URL
     parse_var()
-    api=tradeapi.REST(key_id=API_KEY_ID,secret_key=API_SECRET_KEY,api_version="v2",base_url=BASE_URL)
-    api.submit_order(symbol="AMD",qty="1",side="buy",type="market",time_in_force="day")
-    
+    while True:
+        try:
+            stocks_list = ["AAPL", "MSFT","TSLA"]  
+            st = timezone('EST')
+            if tools.is_trading_hours():
+                for stock in stocks_list:
+                    now = datetime.datetime.now(st)
+                    current_stock = stock
+                    print(f"trading hours is on!! stock is: {current_stock}")
+                    api=tradeapi.REST(key_id=API_KEY_ID,secret_key=API_SECRET_KEY,api_version="v2",base_url=BASE_URL)
+                    tools.wait_time(300)
+
+                    if not tools.is_trading_hours():
+                        break
+
+            if not tools.is_trading_hours():
+                now = datetime.datetime.now(st)
+                print(f"Turns out it isn't trading time. Time is: {now}")
+                tools.wait_time(seconds=1200)
+
+        except Exception as e:
+            # Save error in .txt file
+            with open("log.txt", 'a') as f:  # Use file to refer to the file object
+
+                f.write("\n \n")
+                f.write(f"Stock ticker was {current_stock}")
+                f.write("\n \n")
+                f.write(f"Time is: {datetime.datetime.now()}")
+                f.write(f"'exception_type: '{type(e).__name__}, \nerror_reason': {e.args}, \n\n")
+                f.write(f"Traceback is: {traceback.format_exc()}")
+                print(f"Breaking because of an error: {e}")
+
+
+
+
+def trading_strategy_1(api, stock):
+    account = api.get_account()
+    currently_own_this_stock = tools.currently_own_this_stock(api, stock)
+
 
 def parse_var(f="keys.txt"):
     global API_KEY_ID, API_SECRET_KEY, BASE_URL
